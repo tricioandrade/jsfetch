b4b072c7303c6bda402f62320e85032f58f507cc
tricioandrade <tricioandrade@outlook.com>
2022-04-19 11:51:57 +0100

    update

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 import {request} from './resource/app.js';
-
 let box = document.querySelector('div.todo ul');
-
 const req = new request('https://jsonplaceholder.typicode.com/todos');
 
-req.get()
-.then(data => {
-	console.log(data);
-	data.forEach(element => {
+
+
+function listTodos(todos) {
+
+	todos.forEach(element => {
 		console.log(element)
-		box.innerHTML +=`<li class='row shadow-lg rounded mb-3 p-2'>
-							<span class='col-1'><i class='fa fa-user-id'></i></span>
+		box.innerHTML +=`<li class='row shadow-lg rounded mb-3 p-2 list-group-item'>
 							<span class='col-2'>${element.id}</span>
-							<span class='col-9'>${element.title}</span>
+							<span class='col-10'>${element.title}</span>
 						</li>`;
 	});
+}
+
+req.get()
+	.then(data => {
+		box.innerHTML = `<span class='preloader'></span>`;		
+		box.innerHTML = ``;
+		listTodos(data);
 })
 .catch(erro => box.innerHTML = erro);
