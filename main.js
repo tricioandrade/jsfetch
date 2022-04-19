5c20f78ef081ab4d5e59b22592b6d1b95d2c18c5
tricioandrade@outlook.com <tricioandrade@outlook.com>
2022-04-19 22:49:35 +0100

    added envent listenner and preloader

--- main.js
+++ main.js
@@ -3,9 +3,21 @@ let box = document.querySelector('div.todo ul');
 const req = new request('https://jsonplaceholder.typicode.com/todos');
 
 
+window.addEventListener('load', () => {
+  box.innerHTML = `<span class='preloader'></span>`;
+  
+  req.get()
+    .then(data => {
+      box.innerHTML = ``;
+      listTodos(data);
+    })
+    .catch(erro => box.innerHTML = erro);
+});
+
+
 
 function listTodos(todos) {
-
+  
 	todos.forEach(element => {
 		console.log(element)
 		box.innerHTML +=`<li class='row shadow-lg rounded mb-3 p-2 list-group-item'>
@@ -13,12 +25,5 @@ function listTodos(todos) {
 							<span class='col-10'>${element.title}</span>
 						</li>`;
 	});
+  
 }
-
-req.get()
-	.then(data => {
-		box.innerHTML = `<span class='preloader'></span>`;		
-		box.innerHTML = ``;
-		listTodos(data);
-})
-.catch(erro => box.innerHTML = erro);
